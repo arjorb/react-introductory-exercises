9b16908bfd538bba44fdb25f1ff62a13ffa7d13c
John U <arjorb@gmail.com>
2023-03-09 22:58:24 +0200

    done working on the handleChange function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [formData, setFormData] = useState({
+    firstname: '',
+    lastname: '',
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormData(prevFormDate => ({ ...prevFormDate, [name]: value }));
+  };
   return (
     <div className='flex justify-center mt-10'>
       <div className='w-80 space-y-3'>
-        <input type='text' className='w-full border border-slate-200 py-1 rounded-sm outline-none pl-1' />
-        <input type='text' className='w-full border border-slate-200 py-1 rounded-sm outline-none pl-1' />
-        <button className='bg-green-500 w-full py-3 rounded-sm text-xl font-semibold text-white'>Greet Me</button>
+        <input
+          type='text'
+          name='firstname'
+          value={formData.firstname}
+          onChange={handleChange}
+          placeholder='First name'
+          className='w-full border border-slate-200 py-1 rounded-sm outline-none pl-1'
+        />
+        <input
+          type='text'
+          name='lastname'
+          value={formData.lastname}
+          onChange={handleChange}
+          placeholder='Second name'
+          className='w-full border border-slate-200 py-1 rounded-sm outline-none pl-1'
+        />
+        <button className='bg-green-500 w-full py-3 rounded-sm text-xl font-medium text-white'>Greet Me</button>
       </div>
     </div>
   );
