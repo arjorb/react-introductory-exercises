4f272dd3457b9c5f23aab17d170bb278336cb06c
John U <arjorb@gmail.com>
2023-03-09 17:06:17 +0200

    creating a state and give it the values

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { useEffect } from 'react';
 const App = () => {
+  const [animals, setAnimals] = useEffect(['🐶 dog', '🐈 cat', '🐣 chicken', '🐄 cow', '🐑 sheep', '🐴 horse']);
+
   return <h1 className='text-3xl text-[#017880] font-bold flex justify-center'>React Intoductory Exercise!</h1>;
 };
 
