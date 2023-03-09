18c0e3c788f44ef0297d33ba3c2ecc0891ea1308
John U <arjorb@gmail.com>
2023-03-09 23:01:40 +0200

    done working on the handleSubmit function

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ const App = () => {
     const { name, value } = e.target;
     setFormData(prevFormDate => ({ ...prevFormDate, [name]: value }));
   };
+
+  const handleSubmit = () => {
+    alert(`Hello, ${formData.firstname} ${formData.lastname} !`);
+  };
   return (
     <div className='flex justify-center mt-10'>
       <div className='w-80 space-y-3'>
@@ -29,7 +33,9 @@ const App = () => {
           placeholder='Second name'
           className='w-full border border-slate-200 py-1 rounded-sm outline-none pl-1'
         />
-        <button className='bg-green-500 w-full py-3 rounded-sm text-xl font-medium text-white'>Greet Me</button>
+        <button onClick={handleSubmit} className='bg-green-500 w-full py-3 rounded-sm text-xl font-medium text-white'>
+          Greet Me
+        </button>
       </div>
     </div>
   );
