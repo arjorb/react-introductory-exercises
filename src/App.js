aece62abafcc323eb2dc138826b48e5d725e0b1b
John U <arjorb@gmail.com>
2023-03-09 22:47:55 +0200

    creating the input field and the form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 const App = () => {
-  return <h1 className='text-3xl text-[#017880] font-bold flex justify-center'>React Intoductory Exercise!</h1>;
+  return (
+    <div className='flex justify-center mt-10'>
+      <div className='w-80 space-y-3'>
+        <input type='text' className='w-full border border-slate-200 py-1 rounded-sm outline-none pl-1' />
+        <input type='text' className='w-full border border-slate-200 py-1 rounded-sm outline-none pl-1' />
+        <button className='bg-green-500 w-full py-3 rounded-sm text-xl font-semibold text-white'>Greet Me</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
