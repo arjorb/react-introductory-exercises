787f959e774f643cda4570756dc4b77a901db772
John U <arjorb@gmail.com>
2023-03-09 16:26:19 +0200

    update the button style

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div className='flex flex-col items-center mt-10 space-y-5'>
       <h1 className='text-lg font-medium'>Button has been clicked: {count} times</h1>
-      <button onClick={handleCount} className='bg-green-500 py-2 px-7 rounded-sm w-max text-white'>
+      <button onClick={handleCount} className='bg-green-500 py-2 px-8 rounded-sm w-max text-white'>
         Click Me
       </button>
     </div>
