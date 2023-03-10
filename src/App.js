e9edbf5452803b9bf61d9463411f293b11fe98df
John U <arjorb@gmail.com>
2023-03-10 10:36:33 +0200

    done adding the button to fetch the requests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 const App = () => {
-  return <h1 className='text-3xl text-[#017880] font-bold flex justify-center'>React Intoductory Exercise!</h1>;
+  return (
+    <h1 className='bg-gray-100 min-h-screen flex flex-col py-10'>
+      <button className='bg-green-500 text-white w-max mx-auto py-3 px-10 text-lg font-medium rounded-md hover:bg-green-600 duration-300'>
+        Fetch Random
+      </button>
+    </h1>
+  );
 };
 
 export default App;
