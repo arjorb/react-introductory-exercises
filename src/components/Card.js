c97c7858c810c57eb3fca28d35abd93c2df2fe89
John U <arjorb@gmail.com>
2023-03-10 16:33:50 +0200

    add some css classes to make it possible to flip

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,17 @@
 const Card = ({ first_name, last_name, avatar, employment }) => {
   return (
-    <div className='w-96 h-96 bg-white rounded-md'>
-      <img src={avatar} alt='avatar' />
-      <div className='pl-6 pt-3 space-y-2'>
-        <h1 className='font-bold'>
-          {first_name} {last_name}
-        </h1>
-        <p className='text-sm'>{employment.title}</p>
+    <div class='card w-96 h-96 bg-white rounded-md relative cursor-pointer'>
+      <div class='front absolute inset-0 bg-white'>
+        <img src={avatar} alt='avatar' />
+        <div className='pl-6 pt-3 space-y-2'>
+          <h1 className='font-bold'>
+            {first_name} {last_name}
+          </h1>
+          <p className='text-sm'>{employment.title}</p>
+        </div>
+      </div>
+      <div class='back absolute inset-0 bg-white flex items-center justify-center rotate-y-180'>
+        <h2 class='text-2xl font-bold'>{employment.key_skill}</h2>
       </div>
     </div>
   );
