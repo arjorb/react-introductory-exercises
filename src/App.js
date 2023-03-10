4219c80bb60885bf733a0a57da964419549a5500
John U <arjorb@gmail.com>
2023-03-10 12:44:15 +0200

    done working on the fetch fundion and the front side of the card

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
+import { click } from '@testing-library/user-event/dist/click';
 import { useEffect, useState } from 'react';
 import Card from './components/Card';
 const App = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getData = async () => {
+    setLoading(true);
+    const res = await fetch('https://random-data-api.com/api/users/random_user?size=10');
+    const data = await res.json();
+    setData(data);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    const getData = async () => {
-      const res = await fetch('https://random-data-api.com/api/users/random_user?size=10');
-      const data = await res.json();
-      setData(data);
-    };
     getData();
   }, []);
 
-  console.log(data);
+  const handleFetch = () => {
+    getData();
+  };
+  const CardElement = data.map(data => <Card key={data.id} {...data} />);
   return (
     <h1 className='bg-gray-100 min-h-screen flex flex-col items-center py-10'>
-      <button className='bg-green-500 text-white w-max mx-auto py-3 px-10 text-lg font-medium rounded-md hover:bg-green-600 duration-300'>
+      <button
+        onClick={handleFetch}
+        className='bg-green-500 text-white w-max mx-auto py-3 px-10 text-lg font-medium rounded-md hover:bg-green-600 duration-300'
+      >
         Fetch Random
       </button>
 
-      <main className='flex flex-wrap justify-center w-10/12 gap-5 mt-10'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </main>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <main className='flex flex-wrap justify-center w-10/12 gap-5 mt-10'>{CardElement}</main>
+      )}
     </h1>
   );
 };
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,15 @@
-const Card = () => {
-  return <div className='w-96 h-96 bg-white'>Card</div>;
+const Card = ({ first_name, last_name, avatar, employment }) => {
+  return (
+    <div className='w-96 h-96 bg-white rounded-md'>
+      <img src={avatar} alt='avatar' />
+      <div className='pl-6 pt-3 space-y-2'>
+        <h1 className='font-bold'>
+          {first_name} {last_name}
+        </h1>
+        <p className='text-sm'>{employment.title}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
