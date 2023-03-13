ffba34661b6e0eb378ecfca3a9a66b5e020a2dc0
John U <arjorb@gmail.com>
2023-03-13 12:22:24 +0200

    remove the console.log

--- src/components/FactCard.js
+++ src/components/FactCard.js
@@ -1,5 +1,4 @@
 const FactCard = ({ setup, punchline }) => {
-  console.log(setup);
   return (
     <div className='bg-white py-14 px-5 shadow-lg rounded-md w-90 text-center'>
       <h1 className='text-xl font-bold pb-4'>{setup}</h1>
