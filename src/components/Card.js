2ad73169d7c9777df1051a44a37d86642647b32b
John U <arjorb@gmail.com>
2023-03-13 12:38:27 +0200

    apply the changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
-const Card = ({ first_name, last_name, avatar, employment }) => {
+const Card = ({ first_name: firstName, last_name: lastName, avatar, employment }) => {
   return (
     <div class='card w-96 h-96 bg-white rounded-md relative cursor-pointer'>
       <div class='front absolute inset-0 bg-white'>
         <img src={avatar} alt='avatar' />
         <div className='pl-6 pt-3 space-y-2'>
           <h1 className='font-bold'>
-            {first_name} {last_name}
+            {firstName} {lastName}
           </h1>
           <p className='text-sm'>{employment.title}</p>
         </div>
